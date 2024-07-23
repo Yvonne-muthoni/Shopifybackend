aa61580ce7e225f4075118ddde27e3c51af9e33d
user.name <akamrannicole@gmail.com>
2024-07-23 19:53:03 +0300

    done

---
+++ app/__init__.py
@@ -0,0 +1,27 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_bcrypt import Bcrypt
+from flask_jwt_extended import JWTManager
+
+db = SQLAlchemy()
+migrate = Migrate()
+bcrypt = Bcrypt()
+jwt = JWTManager()
+
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object('config.Config')
+
+    db.init_app(app)
+    migrate.init_app(app, db)
+    bcrypt.init_app(app)
+    jwt.init_app(app)
+
+    from .routes import main
+    from .auth import auth
+
+    app.register_blueprint(main)
+    app.register_blueprint(auth)
+
+    return app
